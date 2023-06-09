ff9f1f57c141da8dfc7b480c4b2995835113acba
Eldar <eldar_96@bk.ru>
2023-06-09 20:47:24 +0300

    Switched to branch longer

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-        int[] sales = new int[] {100, 200, 300, 400};
+        long[] sales = new long[] {100, 200, 300, 400};
         SalesManager salesManager = new SalesManager(sales);
-        int max = salesManager.max();
+        long max = salesManager.max();
         System.out.println(max);
     }
 }
